5e827a197327b663c03ae206297adcd7ea83b085
JLPM22 <joseluispm2@gmail.com>
2021-12-31 11:05:03 +0100

    spring character controller smooth deltatime + directions

--- Assets/MotionMatching/CharacterController/SpringCharacterController.cs
+++ Assets/MotionMatching/CharacterController/SpringCharacterController.cs
@@ -14,16 +14,20 @@ namespace MotionMatching
         public int NumberPrediction = 3;
         public int PredictionFrames = 20;
         public float MaxSpeed = 1.0f;
-        [Range(0.0f, 1.0f)] public float HalfLife = 0.5f;
+        [Range(0.0f, 1.0f)] public float Responsiveness = 0.75f;
 
         private Vector2 InputMovement;
         private Vector2[] PredictedPosition;
         private Vector2 Velocity;
+        private Vector2[] PredictedVelocity;
         private Vector2 Acceleration;
+        private Queue<float> LastDeltaTime = new Queue<float>();
+        private float SumDeltaTime;
 
         private void Start()
         {
             PredictedPosition = new Vector2[NumberPrediction];
+            PredictedVelocity = new Vector2[NumberPrediction];
             for (int i = 0; i < NumberPrediction; ++i) PredictedPosition[i] = new Vector2(transform.position.x, transform.position.z);
         }
 
@@ -34,39 +38,49 @@ namespace MotionMatching
 
         private void Update()
         {
+            // Average DeltaTime
+            const int nAverageDeltaTime = 20;
+            SumDeltaTime += Time.deltaTime;
+            LastDeltaTime.Enqueue(Time.deltaTime);
+            if (LastDeltaTime.Count == nAverageDeltaTime + 1) SumDeltaTime -= LastDeltaTime.Dequeue();
+
+            // Positions
             Vector2 speed = InputMovement * MaxSpeed;
             Vector2 newPos = new Vector2(transform.position.x, transform.position.z);
             for (int i = 0; i < 2; ++i)
             {
                 // Update Current Position
                 float newX = newPos[i], newVel = Velocity[i], newAcc = Acceleration[i];
-                SpringCharacterUpdate(ref newX, ref newVel, ref newAcc, speed[i], HalfLife, Time.deltaTime);
+                SpringCharacterUpdate(ref newX, ref newVel, ref newAcc, speed[i], 1.0f - Responsiveness, Time.deltaTime);
                 newPos[i] = newX;
                 Velocity[i] = newVel;
                 Acceleration[i] = newAcc;
                 // Predict
                 Span<float> predictPos = stackalloc float[NumberPrediction];
-                SpringCharacterPredict(predictPos,
+                Span<float> predictVel = stackalloc float[NumberPrediction];
+                SpringCharacterPredict(predictPos, predictVel,
                                        NumberPrediction, newPos[i], Velocity[i], Acceleration[i], speed[i],
-                                       HalfLife, Time.deltaTime); // TODO: change Time.deltaTime (only in predict!!!)
-                                                                  // FIRST for an average deltaTime of the last 10frames or more
-                                                                  // if it does not work... use the target framerate (maybe create an fps manager? idk)
-                for (int j = 0; j < NumberPrediction; ++j) PredictedPosition[j][i] = predictPos[j];
+                                       1.0f - Responsiveness, SumDeltaTime / nAverageDeltaTime);
+                for (int j = 0; j < NumberPrediction; ++j)
+                {
+                    PredictedPosition[j][i] = predictPos[j];
+                    PredictedVelocity[j][i] = predictVel[j];
+                }
             }
             transform.position = new Vector3(newPos.x, transform.position.y, newPos.y);
         }
 
         /* https://theorangeduck.com/page/spring-roll-call#controllers */
-        private void SpringCharacterPredict(Span<float> pPose,
+        private void SpringCharacterPredict(Span<float> pPose, Span<float> pVelocity,
                                             int count, float pos, float velocity, float acceleration, float velocityGoal,
                                             float halfLife, float deltaTime)
         {
             for (int i = 0; i < count; ++i)
             {
                 pPose[i] = pos;
-                float v = velocity;
+                pVelocity[i] = velocity;
                 float a = acceleration;
-                SpringCharacterUpdate(ref pPose[i], ref v, ref a, velocityGoal, halfLife, (i + 1) * PredictionFrames * deltaTime);
+                SpringCharacterUpdate(ref pPose[i], ref pVelocity[i], ref a, velocityGoal, halfLife, (i + 1) * PredictionFrames * deltaTime);
             }
         }
         private void SpringCharacterUpdate(ref float pos, ref float velocity, ref float acceleration,
@@ -96,20 +110,22 @@ namespace MotionMatching
         private void OnDrawGizmos()
         {
             const float radius = 0.05f;
-            // Draw Current Position
+            const float vectorReduction = 0.5f;
+            // Draw Current Position & Velocity
             Gizmos.color = new Color(1.0f, 0.3f, 0.1f, 1.0f);
             Gizmos.DrawWireSphere(transform.position, radius);
+            Gizmos.DrawLine(transform.position, transform.position + (new Vector3(Velocity.x, 0.0f, Velocity.y)) * vectorReduction);
 
-            if (PredictedPosition == null) return;
+            if (PredictedPosition == null || PredictedVelocity == null) return;
 
-            // Draw Predicted Position
+            // Draw Predicted Position & Velocity
             Gizmos.color = new Color(0.1f, 0.3f, 1.0f, 1.0f);
             for (int i = 0; i < PredictedPosition.Length; ++i)
             {
-                Vector3 previous = i == 0 ? transform.position : new Vector3(PredictedPosition[i - 1].x, 0.0f, PredictedPosition[i - 1].y);
                 Vector3 current = new Vector3(PredictedPosition[i].x, 0.0f, PredictedPosition[i].y);
+                Vector3 currentVelocity = new Vector3(PredictedVelocity[i].x, 0.0f, PredictedVelocity[i].y);
                 Gizmos.DrawWireSphere(current, radius);
-                Gizmos.DrawLine(previous, current);
+                Gizmos.DrawLine(current, current + currentVelocity * vectorReduction);
             }
         }
 #endif
