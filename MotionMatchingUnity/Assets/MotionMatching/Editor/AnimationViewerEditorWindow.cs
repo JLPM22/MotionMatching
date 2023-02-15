de29d2e6cd885ff304187f214d82a6f6edd2129d
JLPM22 <joseluispm2@gmail.com>
2023-02-15 20:47:59 +0100

    current frame indicator display + interaction

--- MotionMatchingUnity/Assets/MotionMatching/Editor/AnimationViewerEditorWindow.cs
+++ MotionMatchingUnity/Assets/MotionMatching/Editor/AnimationViewerEditorWindow.cs
@@ -9,6 +9,7 @@ using UnityEditor.SceneManagement;
 using UnityEngine.SceneManagement;
 using UnityEditor.PackageManager.UI;
 using System.Runtime.CompilerServices;
+using System;
 
 namespace MotionMatching
 {
@@ -26,12 +27,11 @@ namespace MotionMatching
         private int TargetFramerate;
         private double LastUpdateTime;
 
-        private ObjectField BVHAssetField;
-        private FloatField BVHScaleField;
         private IntegerField CurrentFrameField;
         private IntegerField TargetFramerateField;
 
         private VisualElement Timeline;
+        private VisualElement CurrentFrameIndicator;
 
         [MenuItem("MotionMatching/Animation Viewer")]
         public static void ShowWindow()
@@ -53,41 +53,40 @@ namespace MotionMatching
         public void CreateGUI()
         {
             VisualElement root = rootVisualElement;
-
+            root.RegisterCallback<PointerMoveEvent>(OnPointerMoveRoot, TrickleDown.TrickleDown);
+            root.RegisterCallback<PointerUpEvent>(OnPointerUpRoot, TrickleDown.TrickleDown);
             CreateBVHAssetField(root);
-            CreateScaleField(root);
-            CreateFrameField(root);
-            CreateTimeline(root);
         }
 
         private void CreateBVHAssetField(VisualElement root)
         {
-            BVHAssetField = new ObjectField
+            ObjectField bvhAssetField = new ObjectField
             {
                 label = "BVH Asset",
                 objectType = typeof(TextAsset)
             };
-            BVHAssetField.RegisterValueChangedCallback(x =>
+            bvhAssetField.RegisterValueChangedCallback(x =>
             {
                 BVHAsset = null;
                 if (x.newValue != null && x.newValue != x.previousValue)
                 {
                     BVHAsset = (TextAsset)x.newValue;
+                    CreateBVHFields();
                     ImportBVH();
                 }
             });
-            root.Add(BVHAssetField);
+            root.Add(bvhAssetField);
         }
 
         private void CreateScaleField(VisualElement root)
         {
-            BVHScaleField = new FloatField
+            FloatField bvhScaleField = new FloatField
             {
                 label = "Scale",
                 value = BVHScale,
                 isDelayed = true
             };
-            BVHScaleField.RegisterValueChangedCallback(x =>
+            bvhScaleField.RegisterValueChangedCallback(x =>
             {
                 if (x.newValue != x.previousValue)
                 {
@@ -95,7 +94,7 @@ namespace MotionMatching
                     ImportBVH();
                 }
             });
-            root.Add(BVHScaleField);
+            root.Add(bvhScaleField);
         }
 
         private void CreateFrameField(VisualElement root)
@@ -123,6 +122,8 @@ namespace MotionMatching
             root.Add(TargetFramerateField);
         }
 
+        const float FrameRuleHeight = 20;
+        const float PlayButtonWidth = 40;
         private void CreateTimeline(VisualElement root)
         {
             if (root.Contains(Timeline))
@@ -130,7 +131,7 @@ namespace MotionMatching
                 root.Remove(Timeline);
             }
             // Create frames rule with numbers
-            int maxFrames = Animation == null ? 2 : Animation.Frames.Length;
+            int maxFrames = Animation.Frames.Length;
             Timeline = new VisualElement
             {
                 style =
@@ -153,15 +154,52 @@ namespace MotionMatching
                     flexGrow = 1,
                     flexShrink = 1,
                     flexBasis = new StyleLength(StyleKeyword.Auto),
-                    alignItems = Align.Center,
-                    justifyContent = Justify.Center,
-                    backgroundColor = new Color(0.4f, 0.4f, 0.4f, 1.0f),
-                    color = Color.black,
-                    height=20
+                    alignItems = Align.FlexStart,
+                    justifyContent = Justify.FlexStart,
+                    height=FrameRuleHeight,
                 }
             };
             Timeline.Add(frameRule);
-            
+            Button playButton = new Button
+            {
+                text = "Play",
+                style =
+                {
+                    width = PlayButtonWidth,
+                }
+            };
+            playButton.clicked += () =>
+            {
+                if (playButton.text == "Play")
+                {
+                    playButton.text = "Stop";
+                    EditorApplication.update += UpdateAnimationForward;
+                }
+                else
+                {
+                    playButton.text = "Play";
+                    EditorApplication.update -= UpdateAnimationForward;
+                }
+            };
+            frameRule.Add(playButton);
+            VisualElement frameRuleLabels = new VisualElement
+            {
+                style =
+                {
+                    flexDirection = FlexDirection.Row,
+                    flexGrow = 1,
+                    flexShrink = 1,
+                    flexBasis = new StyleLength(StyleKeyword.Auto),
+                    alignItems = Align.Center,
+                    justifyContent = Justify.FlexStart,
+                    backgroundColor = new Color(0.4f, 0.4f, 0.4f, 1.0f),
+                    height = FrameRuleHeight,
+                    color = Color.black,
+                }
+            };
+            frameRuleLabels.RegisterCallback<PointerDownEvent>(OnPointerDownFrameRuler, TrickleDown.TrickleDown);
+            frameRule.Add(frameRuleLabels);
+            // Add reference labels
             const int maxFramesPerRule = 20;
             int framesStep = Mathf.Max(1, maxFrames / maxFramesPerRule);
             for (int i = 0; i < maxFrames; i += framesStep)
@@ -174,15 +212,77 @@ namespace MotionMatching
                         flexGrow = 1,
                         flexShrink = 1,
                         flexBasis = new StyleLength(StyleKeyword.Auto),
-                        alignItems = Align.FlexStart,
+                        alignItems = Align.Center,
                         justifyContent = Justify.FlexStart,
+                        height = FrameRuleHeight,
                         color = Color.black
                     }
                 };
                 Label frameLabel = new Label(i.ToString());
                 frameLabelBox.Add(frameLabel);
-                frameRule.Add(frameLabelBox);
+                frameRuleLabels.Add(frameLabelBox);
             }
+            // Add current frame indicator
+            CurrentFrameIndicator = new VisualElement
+            {
+                style =
+                {
+                    alignItems = Align.FlexStart,
+                    justifyContent = Justify.FlexStart,
+                    backgroundColor = new Color(0.25f, 0.42f, 0.68f, 1.0f),
+                    height = FrameRuleHeight,
+                    width = 5,
+                    position = Position.Absolute,
+                }
+            };
+            frameRuleLabels.Add(CurrentFrameIndicator);
+            UpdateCurrentFrameIndicator();
+        }
+
+        private bool PointerDownOnFrameRuler = false;
+        private void OnPointerDownFrameRuler(PointerDownEvent e)
+        {
+            if (e.button == 0) // left mouse button pressed
+            {
+                PointerDownOnFrameRuler = true;
+                UpdateFrameFromPointer(e.position);
+            }
+        }
+        private void OnPointerUpRoot(PointerUpEvent e)
+        {
+            if (e.button == 0 && PointerDownOnFrameRuler) // left mouse button released
+            {
+                PointerDownOnFrameRuler = false;
+            }
+        }
+        private void OnPointerMoveRoot(PointerMoveEvent e)
+        {
+            if (PointerDownOnFrameRuler) // left mouse button moved
+            {
+                UpdateFrameFromPointer(e.position);
+            }
+        }
+        private void UpdateFrameFromPointer(Vector2 pointer)
+        {
+            int maxFrames = Animation.Frames.Length;
+            float containerWidth = CurrentFrameIndicator.parent.resolvedStyle.width;
+            float x = CurrentFrameIndicator.parent.WorldToLocal(pointer).x;
+            int frame = Mathf.CeilToInt((x / containerWidth) * maxFrames);
+            UpdateCurrentFrame(frame);
+            UpdateAnimation(forward: false);
+        }
+
+        private void UpdateCurrentFrameIndicator()
+        {
+            int maxFrames = Animation.Frames.Length;
+            float containerWidth = CurrentFrameIndicator.parent.resolvedStyle.width;
+            CurrentFrameIndicator.style.left = CurrentFrame * (containerWidth / maxFrames);
+        }
+
+        private void CreateBVHFields()
+        {
+            CreateScaleField(rootVisualElement);
+            CreateFrameField(rootVisualElement);
         }
 
         private void ImportBVH()
@@ -190,9 +290,9 @@ namespace MotionMatching
             RemoveSkeleton();
             BVHImporter bvhImporter = new BVHImporter();
             Animation = bvhImporter.Import(BVHAsset, BVHScale);
+            CreateTimeline(rootVisualElement);
             UpdateTargetFramerate(Mathf.CeilToInt(1.0f / Animation.FrameTime));
             UpdateCurrentFrame(0);
-            CreateTimeline(rootVisualElement);
             // Create skeleton
             Skeleton = new Transform[Animation.Skeleton.Joints.Count];
             for (int j = 0; j < Skeleton.Length; j++)
@@ -234,6 +334,7 @@ namespace MotionMatching
                     capsule.localRotation = Quaternion.Inverse(t.parent.rotation) * Quaternion.LookRotation(new Vector3(-up.y, up.x, 0.0f), up);
                 }
             }
+            UpdateAnimation(forward: false);
         }
 
         private void UpdateTargetFramerate(int newValue)
@@ -244,8 +345,10 @@ namespace MotionMatching
 
         private void UpdateCurrentFrame(int newValue)
         {
+            newValue = Mathf.Clamp(newValue, 0, Animation.Frames.Length - 1);
             CurrentFrame = newValue;
             CurrentFrameField.value = CurrentFrame;
+            UpdateCurrentFrameIndicator();
         }
         
         private void RemoveSkeleton()
@@ -264,7 +367,8 @@ namespace MotionMatching
             }
         }
 
-        private void Update()
+        private void UpdateAnimationForward() => UpdateAnimation();
+        private void UpdateAnimation(bool forward=true)
         {
             if (Animation != null && LastUpdateTime + (1.0 / TargetFramerate) < EditorApplication.timeSinceStartup)
             {
@@ -276,7 +380,7 @@ namespace MotionMatching
                     Skeleton[i].localRotation = frame.LocalRotations[i];
                 }
                 // Update frame index
-                UpdateCurrentFrame((CurrentFrame + 1) % Animation.Frames.Length);
+                if (forward) UpdateCurrentFrame((CurrentFrame + 1) % Animation.Frames.Length);
                 LastUpdateTime = EditorApplication.timeSinceStartup;
             }
         }
@@ -284,6 +388,7 @@ namespace MotionMatching
         private void OnDestroy()
         {
             ReturnButton.ReturnScene();
+            EditorApplication.update -= UpdateAnimationForward;
         }
     }
 
